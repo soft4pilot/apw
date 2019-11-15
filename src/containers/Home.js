35945768749f6ec8566626470b7e86e6ebc5339b
José Esteva <josesteva@cic.unam.mx>
2019-11-14 19:30:03 -0600

    Estuve trabajando (sin éxito) en el módulo de acceso

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,20 +2,22 @@
 
 import React from 'react';
 import useData from 'access';
+import useResource from 'services';
 
 import Title from 'components/atoms/Title';
 
-// Utilizar: const Home = props => (
+// Utilizar: const Home = props => {
 // ...
 // );
 
-function Home() {
+const Home = () => {
 
   // Obener los datos del recurso producto
   const product = useData('product', {});
 
   return (
     <div>
+      <h1></h1>
       <Title text={product.name} />
       <hr />
     </div>

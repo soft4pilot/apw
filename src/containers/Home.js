63435567debb8104fea5ba056b24326be72f7f66
José Esteva <josesteva@cic.unam.mx>
2019-11-22 12:59:23 -0600

    Agregué manejo de eventos a los contenedores
    Sólo agregué la estructura; sin embargo, los manejadores todavía no 
    funcionan.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,13 +10,19 @@ const Home = () => {
   // Obener los datos del recurso producto
   const product = useResource('product', {});
 
+  // Manejo de eventos
+  const createProduct = () => product.create(null);
+  const updateProduct = () => product.update(null);
+  const removeProduct = () => product.remove(null);
+
   return (
     <div>
       <h1></h1>
       <Title text={product.data.name} />
-      <button onClick={() => product.create}>Crear</button>
-      <button onClick={() => product.update}>Actualizar</button>
-      <button onClick={() => product.remove}>Borrar</button>
+      <h2>{product.data.version}</h2>
+      <button onClick={createProduct}>Crear</button>
+      <button onClick={updateProduct}>Actualizar</button>
+      <button onClick={removeProduct}>Borrar</button>
       <hr />
     </div>
   );

ecba1112220195808d43caa8f20f16b021a4fac2
José Esteva <josesteva@cic.unam.mx>
2020-06-03 12:06:02 -0500

    Agregué un fondo obscuro al menú al desplazarse haca abajo
    Lo hice siguiendo el curso de "React for Designers" de Meng To 
    (design+code).

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { Link } from 'react-router-dom';
 
 // Estilos del Componente
@@ -7,9 +7,26 @@ import Style from './style.module.css';
 // Componente exportado
 const Header = props => {
 
-  // Estructura
+  const [scrolled, setScrolled] = useState(false);
+
+  // Agregar un manejador al evento 'scroll'
+  useEffect(() => {
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, []);
+
+  // Gestionar el evento 'scroll'
+  const handleScroll = e => {
+
+     setScrolled(window.pageYOffset > 40 ? true: false);
+
+ };
+
+  // Estructura del componente
   return (
-    <div className={Style.Header}>
+    <div className={scrolled ? [Style.Header, Style.HeaderScrolled].join(' ') : Style.Header }>
       <div className={Style.HeaderGroup}>
         <Link to="/"><img src="http://placehold.it/60x60" /></Link>
         <Link to="/page">Uno</Link>

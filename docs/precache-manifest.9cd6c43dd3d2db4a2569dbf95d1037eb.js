806e3191db1290fb74df0e4278a5378994312645
soft4pilot <63612233+soft4pilot@users.noreply.github.com>
2020-11-06 20:43:53 +0000

    Deploying to master from  @ 9575f744bcdeb6deb6325f39ba2209a6ab2166eb 🚀

---
+++ docs/precache-manifest.9cd6c43dd3d2db4a2569dbf95d1037eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff04613c7a1aa25fdc4242403aace152",
+    "url": "/apw/index.html"
+  },
+  {
+    "revision": "6e97639c3a9ce5a0312e",
+    "url": "/apw/static/js/2.b9801359.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/apw/static/js/2.b9801359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89efacbcedfaf4445d14",
+    "url": "/apw/static/js/main.46867421.chunk.js"
+  },
+  {
+    "revision": "ea37f3c014ed1500fe1b",
+    "url": "/apw/static/js/runtime-main.803d90ec.js"
+  },
+  {
+    "revision": "c997fbe0495afdc0004077a42c97f814",
+    "url": "/apw/static/media/background_2.c997fbe0.jpg"
+  }
+]);

596e6f57fb3f8130829465fe2b05a5cec8cd2010
José Esteva <josesteva@soft4pilot.com.mx>
2020-12-01 13:41:09 -0600

    Agregué la lista de tecnologías

--- src/components/Brands/index.js
+++ src/components/Brands/index.js
@@ -3,21 +3,19 @@
 // Encabezado (Header)
 
 import React from 'react';
-import Styled from 'styled-components';
+import Styled, { ThemeContext } from 'styled-components';
 
-// Acceso al modelo de datos
 import useQuery from 'model/queries';
-
-import Feature from '../Feature';
+import { Brand } from '@soft4pilot/sdb';
 
 // URL de la API
 const url = 'https://api-us-west-2.graphcms.com/v2/ckhbgkjs3afpk01z7dnqocy9j/master'; // TEMPORAL: La URL de la API debe estar en el contexto de la aplicación
 
 // Consulta de datos
 const query = `query {
-  features {
+  technologies {
     icon,
-    title
+    url
   }
 }`;
 
@@ -41,15 +39,17 @@ const Content = Styled.span`
 `;
 
 // Componente
-const Features = props => {
+const Brands = props => {
+
+  const theme = React.useContext(ThemeContext);
 
   const [{loading, error, data}, setQuery] = useQuery(url, query);
 
   return (
     <Container>
-      { data && (data.features.map((feature, i) => (
+      { data && (data.technologies.map((technology, i) => (
         <Content>
-          <Feature key={i} icon={feature.icon} title={feature.title} />
+          <Brand icon="circle" size='80px' color={theme.color.primary.lighter}  href={technology.url} />
         </Content>
       )))}
     </Container>
@@ -57,4 +57,4 @@ const Features = props => {
 
 }
 
-export default Features;
+export default Brands;

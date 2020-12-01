48ee7e6d6692c9d86aa3350abb0e8739858dbb11
José Esteva <josesteva@soft4pilot.com.mx>
2020-11-30 22:19:42 -0600

    Agregué un componente de característica (Feature)
    En realidad lo moví desde el sistema de diseño.

---
+++ src/components/Feature/index.js
@@ -0,0 +1,81 @@
+// SBD 1.0
+// Átomos
+// Feature (Característica)
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styled, { ThemeContext } from 'styled-components';
+
+import { Icon } from '@soft4pilot/sdb';
+import { Text } from '@soft4pilot/sdb';
+
+// Estilo
+const Container = Styled.div`
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: ${props => props.theme.space.m} 0;
+
+  width: 184px;
+  /* height: auto; */
+
+  background: ${props => props.theme.color.secondary.lighter};
+  border: 1px solid ${props => props.theme.color.secondary.lightest};
+  border-radius: 8px;
+  /* box-sizing: border-box; */
+  box-shadow: 0px ${props => props.theme.effect.shadow.Y} ${props => props.theme.effect.shadow.blur} ${props => props.theme.color.primary.darker};
+
+  color: ${props => props.theme.color.primary.dark};
+
+  p {
+
+    margin-top: ${props => props.theme.space.ms};
+
+    color: ${props => props.theme.color.primary.darkest};
+
+    font-family: ${props => props.theme.font.family.text};
+    font-style: normal; /* Quizá falta esta variabe */
+    font-weight: ${props => props.theme.font.weight.regular};
+    font-size: ${props => props.theme.font.size.text.small};
+    line-height: ${props => props.theme.font.height.text};
+
+    text-align: center;
+
+  }
+
+  * {
+    flex: none;
+    order: 1;
+    align-self: center;
+    flex-grow: 0;
+    margin: 0px ${props => props.theme.space.ms};
+  }
+
+`;
+
+// Componente
+const Feature = props => {
+
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <Container>
+      <Icon name={props.icon} size={theme.space.xl} color={theme.color.primary.dark}/>
+      <Text>{props.text}</Text>
+    </Container>
+  )
+
+}
+
+// Parámetros
+Feature.propTypes = {
+  icon: PropTypes.string,
+}
+
+// Valores predeterminados
+Feature.defaultProps = {
+  icon: 'circle',
+}
+
+export default Feature;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,17 +3,11 @@
 // Encabezado (Header)
 
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import Styled from 'styled-components';
+import Styled, { ThemeContext } from 'styled-components';
 
 import { Metadata } from 'model/metadata';
 
-import { ReactComponent as Logo } from 'assets/soft4pilot.svg';
-
-// Componetes utilizados
-// import Brand from './Brand';
-// import Menu from './Menu';
-
+import { Brand } from '@soft4pilot/sdb';
 import { Menu } from '@soft4pilot/sdb';
 
 // Estilos generales
@@ -30,23 +24,16 @@ const Container = Styled.header`
 
 `;
 
-// TEMPORAL: Cambiar el color y tamaño del logotipo. Sin embargo, estos estilos vendrán del tema "Dark" del sistema de diseño.
-const StyledLogo = Styled(Logo)`
-
-  color: ${props => props.theme.color.base.white};
-  font-size: ${props => props.theme.font.size.display.large};
-
-`;
-
 // Componente exportado
 const Header = props => {
 
+  const theme = React.useContext(ThemeContext);
   const [{resources}] = React.useContext(Metadata);
 
   // Estructura
   return (
     <Container>
-      <StyledLogo />
+      <Brand icon="soft4pilot" color={theme.color.base.white} size={theme.font.size.display.large} />
       <Menu items={resources} inverted />
     </Container>
   );

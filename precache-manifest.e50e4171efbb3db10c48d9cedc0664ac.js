09418957c255a8d4df96c4eb7944124b199c9644
soft4pilot <63612233+soft4pilot@users.noreply.github.com>
2020-12-04 23:26:10 +0000

    Deploying to gh-pages from  @ b3ec9b8566bafa8a945c7b7c816031eeec953a95 🚀

---
+++ precache-manifest.e50e4171efbb3db10c48d9cedc0664ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20635a3b2e4412e4413ca14ec3eaf1a",
+    "url": "/apw/index.html"
+  },
+  {
+    "revision": "490430a2c2b8e55b07eb",
+    "url": "/apw/static/js/2.7df4b79d.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/apw/static/js/2.7df4b79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f3bf6589a82a07f4db",
+    "url": "/apw/static/js/main.02c09a0f.chunk.js"
+  },
+  {
+    "revision": "ea37f3c014ed1500fe1b",
+    "url": "/apw/static/js/runtime-main.803d90ec.js"
+  },
+  {
+    "revision": "ec8bc879ae33ea8e8473faf1a28e8a87",
+    "url": "/apw/static/media/icons.ec8bc879.svg"
+  },
+  {
+    "revision": "a4a49cf31d888de2c9d7f0a327f0bd34",
+    "url": "/apw/static/media/logos.a4a49cf3.svg"
+  }
+]);
